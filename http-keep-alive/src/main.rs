57b36ed86679987bd27b3b10f30460a4a1af920d
meowjesty <meowjesty_nyan@outlook.com>
2023-09-06 15:18:41 -0300

    add test image for issue 1317: mirror existing session

---
+++ http-keep-alive/src/main.rs
@@ -0,0 +1,23 @@
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_default_env()
+        .format_timestamp_secs()
+        .write_style(env_logger::WriteStyle::Never)
+        .init();
+
+    HttpServer::new(|| App::new().service(index).wrap(Logger::default()))
+        .bind(("0.0.0.0", 80))?
+        .keep_alive(Duration::from_secs(240))
+        .run()
+        .await
+}
+
+#[get("/")]
+#[tracing::instrument(level = "info", ret)]
+async fn index(incoming: String) -> String {
+    format!("Echo [remote]: {incoming}")
+}
+
+use std::time::Duration;
+
+use actix_web::{get, middleware::Logger, App, HttpServer};
