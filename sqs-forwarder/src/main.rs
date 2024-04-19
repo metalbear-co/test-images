26baef91b74ec4f201c9102c157bea99fcd667a8
t4lz <t4lz.git@gmail.com>
2024-04-19 14:15:06 +0200

    SQS test app, currently just prints messages.

---
+++ sqs-forwarder/src/main.rs
@@ -0,0 +1,49 @@
+use aws_sdk_sqs::Client;
+use aws_sdk_sqs::operation::receive_message::ReceiveMessageOutput;
+use aws_sdk_sqs::types::Message;
+
+/// Name of the environment variable that holds the name of the SQS queue to read from.
+const READ_QUEUE_NAME_ENV_VAR: &str = "SQS_TEST_Q_NAME";
+
+#[tokio::main]
+async fn main() {
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    let read_q_name = std::env::var(READ_QUEUE_NAME_ENV_VAR).unwrap();
+    let read_q_url = client
+        .get_queue_url()
+        .queue_name(read_q_name)
+        .send()
+        .await
+        .unwrap()
+        .queue_url
+        .unwrap();
+    let receive_message_request = client
+        .receive_message()
+        // Without this the wait time would be 0 and responses would return immediately also when
+        // there are no messages (and potentially even sometimes return empty immediately when
+        // there are actually messages).
+        // By setting a time != 0 (20 is the maximum), we perform "long polling" which means we
+        // won't get "false empties" and also less empty responses, because SQS will wait for that
+        // time before returning an empty response.
+        .wait_time_seconds(20)
+        .queue_url(&read_q_url);
+    loop {
+        let res = receive_message_request.clone().send().await.unwrap();
+        println!("ReceiveMessageOutput: {res:?}"); // TODO: remove this
+        if let ReceiveMessageOutput{messages: Some(messages),..} = res {
+            for Message {
+                body,
+                message_attributes,
+                message_id,
+                receipt_handle,
+                ..
+            } in messages {
+                println!("message_id: {message_id:?}, message_attributes: {message_attributes:?}, body: {body:?}");
+                if let Some(handle) = receipt_handle {
+                    client.delete_message().queue_url(&read_q_url).receipt_handle(handle).send().await.unwrap();
+                }
+            }
+        }
+    }
+}
