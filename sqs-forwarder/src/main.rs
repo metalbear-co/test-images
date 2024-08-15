f62de83edb1477c5d95945aeebc009acbef95f36
t4lz <t4lz.git@gmail.com>
2024-08-15 17:36:59 +0200

    add another queue, fifo queue

--- sqs-forwarder/src/main.rs
+++ sqs-forwarder/src/main.rs
@@ -1,23 +1,19 @@
-use aws_sdk_sqs::Client;
-use aws_sdk_sqs::operation::receive_message::ReceiveMessageOutput;
-use aws_sdk_sqs::types::Message;
+use aws_sdk_sqs::{operation::receive_message::ReceiveMessageOutput, types::Message, Client};
 use tokio::time::{sleep, Duration};
 
 /// Name of the environment variable that holds the name of the SQS queue to read from.
-const READ_QUEUE_NAME_ENV_VAR: &str = "SQS_TEST_Q_NAME";
+const QUEUE_NAME_ENV_VAR: &str = "SQS_TEST_Q_NAME";
 
-#[tokio::main]
-async fn main() {
-    let shared_config = aws_config::load_from_env().await;
-    let client = Client::new(&shared_config);
-    let read_q_name = std::env::var(READ_QUEUE_NAME_ENV_VAR).unwrap();
+/// Name of the environment variable that holds the name of the SQS fifo queue to read from.
+const FIFO_QUEUE_NAME_ENV_VAR: &str = "SQS_TEST_FIFO_Q_NAME";
+
+async fn read_from_queue(read_q_name: String, client: Client) -> Result<(), anyhow::Error> {
     println!("Q Name: {read_q_name}");
     let read_q_url = client
         .get_queue_url()
         .queue_name(read_q_name)
         .send()
-        .await
-        .unwrap()
+        .await?
         .queue_url
         .unwrap();
     let receive_message_request = client
@@ -37,22 +33,45 @@ async fn main() {
             Err(err) => {
                 println!("ERROR: {err:?}");
                 sleep(Duration::from_secs(3)).await;
-                continue
+                continue;
             }
         };
-        if let ReceiveMessageOutput{messages: Some(messages),..} = res {
+        if let ReceiveMessageOutput {
+            messages: Some(messages),
+            ..
+        } = res
+        {
             for Message {
                 body,
                 message_attributes,
                 message_id,
                 receipt_handle,
                 ..
-            } in messages {
+            } in messages
+            {
                 println!("message_id: {message_id:?}, message_attributes: {message_attributes:?}, body: {body:?}");
                 if let Some(handle) = receipt_handle {
-                    client.delete_message().queue_url(&read_q_url).receipt_handle(handle).send().await.unwrap();
+                    client
+                        .delete_message()
+                        .queue_url(&read_q_url)
+                        .receipt_handle(handle)
+                        .send()
+                        .await?;
                 }
             }
         }
     }
+}
+
+#[tokio::main]
+async fn main() {
+    let sdk_config = aws_config::load_from_env().await;
+    let client = Client::new(&sdk_config);
+    let read_q_name = std::env::var(QUEUE_NAME_ENV_VAR).unwrap();
+    let q_task_handle = tokio::spawn(read_from_queue(read_q_name.clone(), client.clone()));
+    let read_q_name = std::env::var(FIFO_QUEUE_NAME_ENV_VAR).unwrap();
+    let fifo_q_task_handle = tokio::spawn(read_from_queue(read_q_name.clone(), client.clone()));
+    let (q_res, fifo_res) = tokio::join!(q_task_handle, fifo_q_task_handle);
+    q_res.unwrap().unwrap();
+    fifo_res.unwrap().unwrap();
 }
