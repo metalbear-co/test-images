e5e61f412348a4981b3c92b4e682e877fc48e4a5
Dmitry Dodzin <d.dodzin@gmail.com>
2026-03-26 18:18:10 +0200

    Update

--- rabbitmq-worker/src/inventory/inventory.controller.ts
+++ rabbitmq-worker/src/inventory/inventory.controller.ts
@@ -1,10 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
 
 import type { InventoryProductRequest, InventoryReserveRequest } from './inventory.interfaces';
 
 @Controller()
 export class InventoryController {
+  private readonly logger = new Logger(InventoryController.name);
+
   private stock = {
     Laptop: 10,
     Phone: 20,
@@ -12,11 +14,15 @@ export class InventoryController {
 
   @MessagePattern('inventory.check')
   checkInventory(data: InventoryProductRequest) {
+    this.logger.log(`checking inventory: product -> ${data.product} quantity -> ${data.quantity}`)
+
     return this.stock[data.product] >= data.quantity;
   }
 
   @MessagePattern('inventory.reserve')
   reserve(data: InventoryReserveRequest) {
+    this.logger.log(`reserving inventory: product -> ${data.product} quantity -> ${data.quantity}`)
+
     if (this.stock[data.product] >= data.quantity) {
       this.stock[data.product] -= data.quantity;
       return true;
